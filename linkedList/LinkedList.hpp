780afa1676f9bb8afb71c6582c2731b37bd51b33
Michael Bechtel <m783b224@ku.edu>
2016-02-05 15:34:18 -0600

    Add comments to the fixed method definitions

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -29,6 +29,7 @@ bool LinkedList<T>::isEmpty() const
 template <typename T>
 int LinkedList<T>::size() const
 {
+	//Returns the size member variable
 	return(m_size);
 }
 
@@ -38,16 +39,21 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
+	//Continues while there are still elements in the list
 	while(temp != nullptr)
 	{
+		//Determines if the value parameter is in the current Node
 		if(value == temp->getValue())
 		{
+			//Changes variable to show the desired value was present
 			isFound = true;
 		}
-
+		
+		//Increments the temp Node to the next Node in the list
 		temp = temp->getNext();
 	}
-
+	
+	//Return if the given value was found
 	return(isFound);
 }
 
@@ -106,31 +112,49 @@ bool LinkedList<T>::removeBack()
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
+	//Determines if there is only one Node in the list
 	if(m_size == 1)
 	{
+		//Deletes the only Node
 		delete m_front;
+	
+		//Changes the pointer to null
 		m_front = nullptr;
+		
+		//Decreases size list by 1
 		m_size--;
+
+		//States that an element was removed from the list
 		isRemoved = true;
 	}
-	else if(m_size > 1)
+	else if(m_size > 1) //Checks to see if there are 2+ elements in the list
 	{
+		//Initializes lastNode equal to the first Node in the list
 		lastNode = m_front;
 
+		//Continues while lastNode is not equal to the last node
 		while(lastNode->getNext() != nullptr)
 		{
+			//Sets both node variables to the next one (from where they are) in the list
 			secondintoLast = lastNode;
 			lastNode = lastNode->getNext();
 		}
 
+		//Sets second to last node's next pointer to null
 		secondintoLast->setNext(nullptr);
+
+		//Deletes last node and sets pointer to null
 		delete lastNode;
 		lastNode = nullptr;
+
+		//Decreases size of list by 1
 		m_size--;
+
+		//States that an element was removed from the list
 		isRemoved = true;
 	}
 	
-
+	//Returns if an element was removed from the list
 	return(isRemoved);
 }	
 
